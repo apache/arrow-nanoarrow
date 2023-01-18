e42906da8a2077aa41556179679a9d088b894aa4
Dewey Dunnington <dewey@voltrondata.com>
2023-01-18 16:10:30 -0400

    update scripts for a more minimal vendoring of flatcc

--- extensions/nanoarrow_ipc/dev/update_vendored_flatcc.sh
+++ extensions/nanoarrow_ipc/dev/update_vendored_flatcc.sh
@@ -20,9 +20,56 @@ curl -L https://github.com/dvidelabs/flatcc/archive/refs/tags/v0.6.1.zip --outpu
 unzip flatcc-src.zip -d flatcc-src
 FLATCC_SRC_DIR=flatcc-src/`ls flatcc-src`
 
+# Remove previous vendored flatcc
 rm -rf ../thirdparty/flatcc
-mv $FLATCC_SRC_DIR ../thirdparty/flatcc
+
+# Create the folders we need to exist
+mkdir -p ../thirdparty/flatcc/src/runtime
+mkdir -p ../thirdparty/flatcc/include/flatcc/portable
+
+# The only part of the runtime we need
+cp $FLATCC_SRC_DIR/src/runtime/emitter.c \
+  $FLATCC_SRC_DIR/src/runtime/builder.c \
+  $FLATCC_SRC_DIR/src/runtime/verifier.c \
+  $FLATCC_SRC_DIR/src/runtime/refmap.c \
+  ../thirdparty/flatcc/src/runtime/
+
+# We also need the headers for those sources. makedepend
+# can get those for us in topological order.
+pushd $FLATCC_SRC_DIR/include
+
+# List object dependencies (warns that it can't find system headers, which is OK)
+makedepend -s#: -f- -- -I. -DFLATCC_PORTABLE -- 2>/dev/null \
+  ../src/runtime/emitter.c \
+  ../src/runtime/builder.c \
+  ../src/runtime/verifier.c \
+  ../src/runtime/refmap.c \
+  ../../../../src/nanoarrow/nanoarrow_ipc.c | \
+  # Remove the '<src file>.o: ' prefix
+  sed 's/[^:]*: *//' | \
+  # Spaces to new lines
+  sed 's/ /\n/' | \
+  # Only unique lines (but don't sort to preserve topological order)
+  awk '!x[$0]++' | \
+  # Remove blank lines
+  sed '/^$/d' | \
+  # Or anything having to do with nanoarrow
+  sed '/nanoarrow/d' > ../../../flatcc-headers.txt
+
+popd
+
+# Copy the headers we need. Using loops because cp --parents is not portable.
+for file in $(cat flatcc-headers.txt | sed /portable/d); do
+    cp "$FLATCC_SRC_DIR/include/$file" ../thirdparty/flatcc/include/flatcc
+done
+for file in $(cat flatcc-headers.txt | sed -n /portable/p); do
+    cp "$FLATCC_SRC_DIR/include/$file" ../thirdparty/flatcc/include/flatcc/portable;
+done
+
+# And the license
+cp $FLATCC_SRC_DIR/LICENSE ../thirdparty/flatcc
 
 # clean up
 rm -rf flatcc-src
 rm flatcc-src.zip
+rm flatcc-headers.txt
