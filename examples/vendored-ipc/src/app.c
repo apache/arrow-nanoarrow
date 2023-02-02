9e84e5d88b2cc02259d13dbb7dc85b1fef2784a0
Dane Pitkin <48041712+danepitkin@users.noreply.github.com>
2023-02-02 12:51:40 -0500

    docs(examples/vendored-ipc): Create vendored IPC extension example (#102)
    ### Issue
    This closes https://github.com/apache/arrow-nanoarrow/issues/100.
    
    ### Description
    Create an example project that vendors the nanoarrow_ipc files.
    
    ### Testing
    1. I copied the commands from
    [.github/workflows/examples.yaml](https://github.com/apache/arrow-nanoarrow/compare/main...danepitkin:arrow-nanoarrow:GH-100?expand=1#diff-c4613dcee560ce1f5feabe2f1795517d67bc2a3464d17d9c0d0abce1a1fe3a68)
    into a script and ran it successfully.
    2. I manually ran all of the commands in
    [examples/vendored-ipc/README.md](https://github.com/apache/arrow-nanoarrow/compare/main...danepitkin:arrow-nanoarrow:GH-100?expand=1#diff-d0a2f92ba29369d25738a82a2c1c155826de0e387f3b0c5c9ad1bc0bf7eec3c9).

---
+++ examples/vendored-ipc/src/app.c
@@ -0,0 +1,42 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "library.h"
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  int64_t in_size = 0;
+  uint8_t data[8096];
+
+  freopen(NULL, "rb", stdin);
+  in_size = fread(data, 1, 8096, stdin);
+
+  if (in_size == 8096) {
+    fprintf(stderr, "This example can't read messages more than 8096 bytes\n");
+    return -1;
+  }
+
+  int result = verify_ipc_message(data, in_size);
+  if (result != 0) {
+    fprintf(stderr, "%s\n", my_library_last_error());
+  }
+
+  return result;
+}
