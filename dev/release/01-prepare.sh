8480ccc956959c89bc939e180229377827c5bca1
Dewey Dunnington <dewey@fishandwhistle.net>
2023-02-24 13:29:32 -0400

    Quality of life improvements in first two release scripts

--- dev/release/01-prepare.sh
+++ dev/release/01-prepare.sh
@@ -76,4 +76,4 @@ git commit -m "chore: update versions for ${version}" --allow-empty
 git tag -a "${release_candidate_tag}" -m "nanoarrow ${version} RC ${rc_number}"
 
 echo "Created release candidate tag: ${release_candidate_tag}"
-echo "Push this tag before continuing!"
+echo "git push upstream ${release_candidate_tag} before continuing!"
