71f66abae013cdc8461cef5d45e1b409f12bf598
Dewey Dunnington <dewey@voltrondata.com>
2023-04-21 12:57:40 -0300

    fix cmake coverage

--- ci/scripts/coverage.sh
+++ ci/scripts/coverage.sh
@@ -17,34 +17,57 @@
 # specific language governing permissions and limitations
 # under the License.
 
+set -e
+set -o pipefail
+
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 NANOARROW_SOURCE_DIR="$(cd "${SOURCE_DIR}/../.." && pwd)"
 
 # Use a sandbox dir for intermediate results. This also makes it easier
 # to debug the process.
-SANDBOX_DIR="${NANOARROW_SOURCE_DIR}/coverage"
+SANDBOX_DIR="${NANOARROW_SOURCE_DIR}/_coverage"
 if [ -d "${SANDBOX_DIR}" ]; then
     rm -rf "${SANDBOX_DIR}"
 fi
 mkdir "${SANDBOX_DIR}"
 
-# Run tests with gcov for core library
-mkdir "${SANDBOX_DIR}/build"
-pushd "${SANDBOX_DIR}/build"
+# Bulid + run tests with gcov for core library
+mkdir "${SANDBOX_DIR}/nanoarrow"
+pushd "${SANDBOX_DIR}/nanoarrow"
 
-popd
-
-# Run tests with gcov for IPC extension
-mkdir "${SANDBOX_DIR}/build-ipc"
-pushd "${SANDBOX_DIR}/build-ipc"
-
-popd
-
-# Run covr::package_coverage() on the R package
-
-
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DNANOARROW_BUILD_TESTS=ON
+cmake "${NANOARROW_SOURCE_DIR}" \
+    -DNANOARROW_BUILD_TESTS=ON -DNANOARROW_CODE_COVERAGE=ON
 cmake --build .
+ctest .
+
+popd
+
+# Build + run tests with gcov for IPC extension
+mkdir "${SANDBOX_DIR}/nanoarrow_ipc"
+pushd "${SANDBOX_DIR}/nanoarrow_ipc"
+
+cmake "${NANOARROW_SOURCE_DIR}/extensions/nanoarrow_ipc" \
+    -DNANOARROW_IPC_BUILD_TESTS=ON -DNANOARROW_IPC_CODE_COVERAGE=ON
+cmake --build .
+ctest .
+
+popd
+
+pushd "${SANDBOX_DIR}"
+
+# Generate coverage.info file for both cmake projects using lcov
+lcov --capture --directory . \
+    --exclude "*_test.cc" \
+    --exclude "/usr/*" \
+    --exclude "*/gtest/*" \
+    --exclude "*/flatcc/*" \
+    --exclude "*_generated.h" \
+    --output-file coverage.info
+
+# Print some debug output
+lcov --list coverage.info
+
+# Generate the html coverage while we're here
+genhtml coverage.info --output-directory html --prefix "${NANOARROW_SOURCE_DIR}"
+
+popd
