8f95866ba5c25ef1ba13752d3a1a8940b24ac464
Dewey Dunnington <dewey@fishandwhistle.net>
2023-09-05 17:18:31 -0300

    align error message

--- extensions/nanoarrow_ipc/src/nanoarrow/nanoarrow_ipc_reader.c
+++ extensions/nanoarrow_ipc/src/nanoarrow/nanoarrow_ipc_reader.c
@@ -271,7 +271,8 @@ static int ArrowIpcArrayStreamReaderNextBody(
   private_data->body.size_bytes += bytes_read;
 
   if (bytes_read != bytes_to_read) {
-    ArrowErrorSet(&private_data->error, "Expected to read %ld bytes but read %ld bytes",
+    ArrowErrorSet(&private_data->error,
+                  "Expected to be able to read %ld bytes for message body but got %ld",
                   (long)bytes_to_read, bytes_read);
     return EIO;
   } else {
