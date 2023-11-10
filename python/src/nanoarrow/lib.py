90fc9dde15aefcae799e0d70f4a1cbaf51aba50e
Dewey Dunnington <dewey@voltrondata.com>
2023-11-10 09:40:47 -0400

    fix isort

--- python/src/nanoarrow/lib.py
+++ python/src/nanoarrow/lib.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from ._lib import Array, ArrayStream, Schema, DeviceArray, Device
+from ._lib import Array, ArrayStream, Device, DeviceArray, Schema
 
 
 def schema(obj):
