94a59442f66a3bb0b9c185c87f45ee5deea86ed5
Dewey Dunnington <dewey@fishandwhistle.net>
2024-02-19 14:08:13 -0400

    maybe fix check

--- r/tests/testthat/test-ipc.R
+++ r/tests/testthat/test-ipc.R
@@ -193,6 +193,7 @@ test_that("read_nanoarrow() reports errors from readBin", {
 test_that("read_nanoarrow() from connection errors when called from another thread", {
   skip_if_not_installed("arrow")
   skip_if_not("dataset" %in% names(arrow::arrow_info()$capabilities))
+  skip_if_not_installed("dplyr")
 
   tf <- tempfile()
   tf_out <- tempfile()
