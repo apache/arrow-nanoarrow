3daa6553c8cf1b3f971965b52332cfadaae7544f
Dewey Dunnington <dewey@voltrondata.com>
2024-02-28 12:59:18 -0400

    rename

--- src/nanoarrow/array_benchmark.cc
+++ src/nanoarrow/array_benchmark.cc
@@ -15,9 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include <algorithm>
-#include <iostream>
-
 #include <benchmark/benchmark.h>
 
 #include "nanoarrow.hpp"
@@ -45,55 +42,98 @@ ArrowErrorCode InitSchemaAndArrayPrimitive(ArrowSchema* schema, ArrowArray* arra
 }
 
 template <typename CType, ArrowType type>
-static void BaseArrayViewGetIntUnsafe(benchmark::State& state) {
+static void BaseArrayViewGetIntUnsafe(benchmark::State& state, double prop_null = 0.0) {
   nanoarrow::UniqueSchema schema;
   nanoarrow::UniqueArray array;
   nanoarrow::UniqueArrayView array_view;
 
   int64_t n_values = 1000000;
+
   std::vector<CType> values(n_values);
   for (int64_t i = 0; i < n_values; i++) {
     values[i] = i % std::numeric_limits<CType>::max();
   }
 
-  int code = InitSchemaAndArrayPrimitive<CType, type>(schema.get(), array.get(), values);
+  std::vector<int8_t> validity;
+
+  if (prop_null > 0) {
+    int64_t num_nulls = n_values * prop_null;
+    int64_t null_spacing = n_values / num_nulls;
+    validity.resize(n_values);
+    for (int64_t i = 0; i < n_values; i++) {
+      validity[i] = i % null_spacing != 0;
+    }
+  }
+
+  int code = InitSchemaAndArrayPrimitive<CType, type>(
+      schema.get(), array.get(), std::move(values), std::move(validity));
   NANOARROW_THROW_NOT_OK(code);
   NANOARROW_THROW_NOT_OK(
       ArrowArrayViewInitFromSchema(array_view.get(), schema.get(), nullptr));
   NANOARROW_THROW_NOT_OK(ArrowArrayViewSetArray(array_view.get(), array.get(), nullptr));
 
   std::vector<CType> values_out(n_values);
-  for (auto _ : state) {
-    for (int64_t i = 0; i < n_values; i++) {
-      values_out[i] = ArrowArrayViewGetIntUnsafe(array_view.get(), i);
+
+  if (prop_null > 0) {
+    for (auto _ : state) {
+      for (int64_t i = 0; i < n_values; i++) {
+        if (ArrowArrayViewIsNull(array_view.get(), i)) {
+          values_out[i] = 0;
+        } else {
+          values_out[i] = ArrowArrayViewGetIntUnsafe(array_view.get(), i);
+        }
+      }
+      benchmark::DoNotOptimize(values_out);
     }
-    benchmark::DoNotOptimize(values_out);
-    benchmark::ClobberMemory();
+  } else {
+    for (auto _ : state) {
+      for (int64_t i = 0; i < n_values; i++) {
+        values_out[i] = ArrowArrayViewGetIntUnsafe(array_view.get(), i);
+      }
+      benchmark::DoNotOptimize(values_out);
+    }
   }
 
   state.SetItemsProcessed(n_values * state.iterations());
 }
 
 static void BM_ArrayViewGetIntUnsafeInt8(benchmark::State& state) {
-  BaseArrayViewGetIntUnsafe<int8_t, NANOARROW_TYPE_INT32>(state);
+  BaseArrayViewGetIntUnsafe<int8_t, NANOARROW_TYPE_INT8>(state);
 }
-
-BENCHMARK(BM_ArrayViewGetIntUnsafeInt8);
 
 static void BM_ArrayViewGetIntUnsafeInt16(benchmark::State& state) {
   BaseArrayViewGetIntUnsafe<int16_t, NANOARROW_TYPE_INT16>(state);
 }
 
-BENCHMARK(BM_ArrayViewGetIntUnsafeInt16);
-
 static void BM_ArrayViewGetIntUnsafeInt32(benchmark::State& state) {
   BaseArrayViewGetIntUnsafe<int32_t, NANOARROW_TYPE_INT32>(state);
 }
-
-BENCHMARK(BM_ArrayViewGetIntUnsafeInt32);
 
 static void BM_ArrayViewGetIntUnsafeInt64(benchmark::State& state) {
   BaseArrayViewGetIntUnsafe<int64_t, NANOARROW_TYPE_INT64>(state);
 }
 
+static void BM_ArrayViewGetIntUnsafeInt8CheckNull(benchmark::State& state) {
+  BaseArrayViewGetIntUnsafe<int8_t, NANOARROW_TYPE_INT8>(state, 0.2);
+}
+
+static void BM_ArrayViewGetIntUnsafeInt16CheckNull(benchmark::State& state) {
+  BaseArrayViewGetIntUnsafe<int16_t, NANOARROW_TYPE_INT16>(state, 0.2);
+}
+
+static void BM_ArrayViewGetIntUnsafeInt32CheckNull(benchmark::State& state) {
+  BaseArrayViewGetIntUnsafe<int32_t, NANOARROW_TYPE_INT32>(state, 0.2);
+}
+
+static void BM_ArrayViewGetIntUnsafeInt64CheckNull(benchmark::State& state) {
+  BaseArrayViewGetIntUnsafe<int64_t, NANOARROW_TYPE_INT64>(state, 0.2);
+}
+
+BENCHMARK(BM_ArrayViewGetIntUnsafeInt8);
+BENCHMARK(BM_ArrayViewGetIntUnsafeInt16);
+BENCHMARK(BM_ArrayViewGetIntUnsafeInt32);
 BENCHMARK(BM_ArrayViewGetIntUnsafeInt64);
+BENCHMARK(BM_ArrayViewGetIntUnsafeInt8CheckNull);
+BENCHMARK(BM_ArrayViewGetIntUnsafeInt16CheckNull);
+BENCHMARK(BM_ArrayViewGetIntUnsafeInt32CheckNull);
+BENCHMARK(BM_ArrayViewGetIntUnsafeInt64CheckNull);
