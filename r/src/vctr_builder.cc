3de01c7749468d6b433615956e715de74e9fd7c0
Dewey Dunnington <dewey@fishandwhistle.net>
2024-02-27 22:16:15 -0400

    with data.frame

--- r/src/vctr_builder.cc
+++ r/src/vctr_builder.cc
@@ -19,6 +19,9 @@
 #include <R.h>
 #include <Rinternals.h>
 
+#include <memory>
+#include <vector>
+
 #include "materialize.h"
 #include "nanoarrow.h"
 #include "nanoarrow/r.h"
@@ -174,6 +177,61 @@ class RcrdBuilder : public VctrBuilder {
  public:
   explicit RcrdBuilder(SEXP ptype_sexp)
       : VctrBuilder(VECTOR_TYPE_DATA_FRAME, ptype_sexp) {}
+
+  ArrowErrorCode Init(const ArrowSchema* schema, VctrBuilderOptions options,
+                      ArrowError* error) override {
+    // Check can convert here
+
+    // Instantiate and initialize children
+    children_.resize(schema->n_children);
+    for (int64_t i = 0; i < schema->n_children; i++) {
+      SEXP child_ptype_sexp;
+      if (ptype_sexp_ != R_NilValue) {
+        child_ptype_sexp = VECTOR_ELT(ptype_sexp_, i);
+      } else {
+        child_ptype_sexp = R_NilValue;
+      }
+
+      VctrBuilder* child = nullptr;
+      NANOARROW_RETURN_NOT_OK(InstantiateBuilder(schema->children[i], child_ptype_sexp,
+                                                 options, &child, error));
+      children_[i].reset(child);
+      NANOARROW_RETURN_NOT_OK(child->Init(schema->children[i], options, error));
+    }
+
+    schema_ = schema;
+    return NANOARROW_OK;
+  }
+
+  SEXP GetPtype() override {
+    if (ptype_sexp_ != R_NilValue) {
+      return ptype_sexp_;
+    }
+
+    SEXP result = PROTECT(Rf_allocVector(VECSXP, schema_->n_children));
+    SEXP result_names = PROTECT(Rf_allocVector(STRSXP, schema_->n_children));
+    for (R_xlen_t i = 0; i < schema_->n_children; i++) {
+      struct ArrowSchema* child = schema_->children[i];
+      if (child->name != NULL) {
+        SET_STRING_ELT(result_names, i, Rf_mkCharCE(child->name, CE_UTF8));
+      } else {
+        SET_STRING_ELT(result_names, i, Rf_mkChar(""));
+      }
+    }
+
+    Rf_setAttrib(result, R_ClassSymbol, nanoarrow_cls_data_frame);
+    Rf_setAttrib(result, R_NamesSymbol, result_names);
+    SEXP rownames = PROTECT(Rf_allocVector(INTSXP, 2));
+    INTEGER(rownames)[0] = NA_INTEGER;
+    INTEGER(rownames)[1] = 0;
+    Rf_setAttrib(result, R_RowNamesSymbol, rownames);
+    UNPROTECT(3);
+    return result;
+  }
+
+ private:
+  const ArrowSchema* schema_;
+  std::vector<std::unique_ptr<VctrBuilder>> children_;
 };
 
 // Currently in infer_ptype.c
