940de24eff5c9835d99c37a3f9568a0684a7745d
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-19 00:20:27 +0000

    Update example to work against an installed nanoarrow.

--- examples/build_tests/build.sh
+++ examples/build_tests/build.sh
@@ -2,9 +2,14 @@
 # This script is used to build the project
 
 # Build (but do not install) nanoarrow.
-cmake -S ../.. -B nanoarrow_build/
+cmake -S ../.. -B nanoarrow_build/ -DCMAKE_INSTALL_PREFIX=nanoarrow_install/
 cmake --build nanoarrow_build/
 
 # Build the project against the built nanoarrow.
 cmake -S . -B build/ -Dnanoarrow_ROOT=nanoarrow_build/
 cmake --build build/
+
+# Install nanoarrow and build against it.
+cmake --install nanoarrow_build/
+cmake -S . -B build_against_install/ -Dnanoarrow_ROOT=nanoarrow_install/
+cmake --build build_against_install/
