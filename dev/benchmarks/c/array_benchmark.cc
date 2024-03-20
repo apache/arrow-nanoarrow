f6a632b93d2de4c26e82ed360321ead217debb5d
Benjamin Kietzman <bengilgit@gmail.com>
2024-03-20 12:24:44 -0400

    review comments

--- dev/benchmarks/c/array_benchmark.cc
+++ dev/benchmarks/c/array_benchmark.cc
@@ -19,6 +19,8 @@
 
 #include <nanoarrow/nanoarrow.hpp>
 
+#include "util.h"
+
 using nanoarrow::Enumerate;
 using nanoarrow::Zip;
 
@@ -103,31 +105,19 @@ static void BaseArrayViewGetInt(benchmark::State& state) {
   int64_t n_values = kNumItemsPrettyBig;
 
   std::vector<CType> values(n_values);
-#if NANOARROW_HAS_ZIP
   for (auto [i, value] : Zip(Enumerate, values)) {
     value = i % std::numeric_limits<CType>::max();
   }
-#else
-  for (int64_t i = 0; i < n_values; i++) {
-    values[i] = i % std::numeric_limits<CType>::max();
-  }
-#endif
 
   NANOARROW_THROW_NOT_OK(
       InitArrayViewFromBuffers(type, array.get(), array_view.get(), {}, values));
 
   std::vector<CType> values_out(n_values);
   for (auto _ : state) {
-#if NANOARROW_HAS_ZIP
     for (auto [i, array_slot, value_out] :
-         Zip(Enumerate, nanoarrow::ViewAs<int32_t>(array_view.get()), values_out)) {
+         Zip(Enumerate, nanoarrow::ViewArrayAs<CType>(array_view.get()), values_out)) {
       value_out = *array_slot;
     }
-#else
-    for (int64_t i = 0; i < n_values; i++) {
-      values_out[i] = ArrowArrayViewGetIntUnsafe(array_view.get(), i);
-    }
-#endif
     benchmark::DoNotOptimize(values_out);
   }
 
@@ -174,20 +164,10 @@ static void BenchmarkArrayViewIsNullNonNullable(benchmark::State& state) {
   // Read the array
   std::vector<int32_t> values_out(n_values);
   for (auto _ : state) {
-#if NANOARROW_HAS_ZIP
     for (auto [i, array_slot, value] :
-         Zip(Enumerate, nanoarrow::ViewAs<int32_t>(array_view.get()), values_out)) {
+         Zip(Enumerate, nanoarrow::ViewArrayAs<int32_t>(array_view.get()), values_out)) {
       value = array_slot.value_or(0);
     }
-#else
-    for (int64_t i = 0; i < n_values; i++) {
-      if (ArrowArrayViewIsNull(array_view.get(), i)) {
-        values_out[i] = 0;
-      } else {
-        values_out[i] = ArrowArrayViewGetIntUnsafe(array_view.get(), i);
-      }
-    }
-#endif
     benchmark::DoNotOptimize(values_out);
   }
 
