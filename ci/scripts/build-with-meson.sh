54cae535eb9e19e9fccd0db7963738dd8f9525cb
Will Ayd <william.ayd@icloud.com>
2024-04-02 08:07:16 -0400

    Try pkg-config

--- ci/scripts/build-with-meson.sh
+++ ci/scripts/build-with-meson.sh
@@ -72,7 +72,7 @@ function main() {
     meson wrap install nlohmann_json
 
     show_header "Compile project with meson"
-    meson setup "${SANDBOX_DIR}"
+    meson setup "${SANDBOX_DIR}" --pkg-config-path "${ARROW_PATH}"
 
     pushd "${SANDBOX_DIR}"
     meson configure -DNANOARROW_BUILD_TESTS=true \
