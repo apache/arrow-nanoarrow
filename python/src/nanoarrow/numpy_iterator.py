60b75ca1fa7b5925a81480e4253d4321c111a92b
Dewey Dunnington <dewey@fishandwhistle.net>
2024-04-22 00:21:53 -0300

    format

--- python/src/nanoarrow/numpy_iterator.py
+++ python/src/nanoarrow/numpy_iterator.py
@@ -1,8 +1,24 @@
-import numpy as np
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
-from nanoarrow.iterator import PyIterator
+import numpy as np
 from nanoarrow._lib import CArrowType
-from nanoarrow.c_lib import c_schema_view, c_array_stream
+from nanoarrow.c_lib import c_array_stream, c_schema_view
+from nanoarrow.iterator import PyIterator
 
 
 def to_numpy_dtype(schema):
@@ -58,7 +74,6 @@ def to_numpy(obj, schema=None, dtype=None):
 
 
 class NumPyIterator(PyIterator):
-
     @classmethod
     def get_dtype_and_iterator(cls, obj, schema=None, dtype=None):
         stream = c_array_stream(obj, schema=schema)
