a17f63b56505227f7c78c1ce4a171c59fef4e64a
Dewey Dunnington <dewey@voltrondata.com>
2024-04-30 16:50:12 -0300

    fix merge

--- python/tests/test_c_buffer_view.py
+++ python/tests/test_c_buffer_view.py
@@ -20,8 +20,8 @@ import pytest
 import nanoarrow as na
 
 
-def test_buffer_view_bool():
-    bool_array_view = na.c_array_view([1, 0, 0, 1], na.bool())
+def test_buffer_view_boolean():
+    bool_array_view = na.c_array_view([1, 0, 0, 1], na.boolean())
     view = bool_array_view.buffer(1)
 
     assert view.element_size_bits == 1
