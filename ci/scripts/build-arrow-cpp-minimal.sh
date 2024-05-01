63c2231244394ae1997b9bf6397fbeb335ab0864
Will Ayd <william.ayd@icloud.com>
2024-05-01 13:26:15 -0400

    Try main for now

--- ci/scripts/build-arrow-cpp-minimal.sh
+++ ci/scripts/build-arrow-cpp-minimal.sh
@@ -45,7 +45,7 @@ ARROW_CPP_SCRATCH_DIR="arrow-cpp-build-${ARROW_CPP_VERSION}"
 mkdir "${ARROW_CPP_SCRATCH_DIR}"
 pushd "${ARROW_CPP_SCRATCH_DIR}"
 
-curl -L "https://www.apache.org/dyn/closer.lua?action=download&filename=arrow/arrow-${ARROW_CPP_VERSION}/apache-arrow-${ARROW_CPP_VERSION}.tar.gz" | \
+curl -L "https://github.com/apache/arrow/archive/refs/heads/main.tar.gz" | \
   tar -zxf -
 mkdir build && cd build
 cmake ../apache-arrow-${ARROW_CPP_VERSION}/cpp \
