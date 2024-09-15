945f595216acc87ab634273ea41aa63b07a9a2d8
Dewey Dunnington <dewey@voltrondata.com>
2024-09-14 20:53:53 -0500

    sketch initial bindings

--- r/src/ipc.c
+++ r/src/ipc.c
@@ -15,6 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <stdint.h>
 #define R_NO_REMAP
 #include <R.h>
 #include <Rinternals.h>
@@ -46,6 +47,50 @@ static SEXP input_stream_owning_xptr(void) {
   R_RegisterCFinalizer(input_stream_xptr, &finalize_input_stream_xptr);
   UNPROTECT(1);
   return input_stream_xptr;
+}
+
+static void finalize_output_stream_xptr(SEXP output_stream_xptr) {
+  struct ArrowIpcOutputStream* output_stream =
+      (struct ArrowIpcOutputStream*)R_ExternalPtrAddr(output_stream_xptr);
+  if (output_stream != NULL && output_stream->release != NULL) {
+    output_stream->release(output_stream);
+  }
+
+  if (output_stream != NULL) {
+    ArrowFree(output_stream);
+  }
+}
+
+static SEXP output_stream_owning_xptr(void) {
+  struct ArrowIpcOutputStream* output_stream =
+      (struct ArrowIpcOutputStream*)ArrowMalloc(sizeof(struct ArrowIpcOutputStream));
+  output_stream->release = NULL;
+  SEXP output_stream_xptr =
+      PROTECT(R_MakeExternalPtr(output_stream, R_NilValue, R_NilValue));
+  R_RegisterCFinalizer(output_stream_xptr, &finalize_output_stream_xptr);
+  UNPROTECT(1);
+  return output_stream_xptr;
+}
+
+static void finalize_writer_xptr(SEXP writer_xptr) {
+  struct ArrowIpcWriter* writer = (struct ArrowIpcWriter*)R_ExternalPtrAddr(writer_xptr);
+  if (writer != NULL && writer->private_data != NULL) {
+    ArrowIpcWriterReset(writer);
+  }
+
+  if (writer != NULL) {
+    ArrowFree(writer);
+  }
+}
+
+static SEXP writer_owning_xptr(void) {
+  struct ArrowIpcWriter* writer =
+      (struct ArrowIpcWriter*)ArrowMalloc(sizeof(struct ArrowIpcWriter));
+  writer->private_data = NULL;
+  SEXP writer_xptr = PROTECT(R_MakeExternalPtr(writer, R_NilValue, R_NilValue));
+  R_RegisterCFinalizer(writer_xptr, &finalize_output_stream_xptr);
+  UNPROTECT(1);
+  return writer_xptr;
 }
 
 SEXP nanoarrow_c_ipc_array_reader_buffer(SEXP buffer_xptr) {
@@ -82,7 +127,7 @@ struct ConnectionInputStreamHandler {
   int return_code;
 };
 
-static SEXP handle_readbin_error(SEXP cond, void* hdata) {
+static SEXP handle_readbin_writebin_error(SEXP cond, void* hdata) {
   struct ConnectionInputStreamHandler* data = (struct ConnectionInputStreamHandler*)hdata;
 
   SEXP fun = PROTECT(Rf_install("conditionMessage"));
@@ -112,6 +157,34 @@ static SEXP call_readbin(void* hdata) {
   return R_NilValue;
 }
 
+static SEXP call_writebin(void* hdata) {
+  struct ConnectionInputStreamHandler* data = (struct ConnectionInputStreamHandler*)hdata;
+
+  // Write 1MB chunks
+  int64_t chunk_buffer_size = 1048576;
+  SEXP chunk_buffer = PROTECT(Rf_allocVector(RAWSXP, chunk_buffer_size));
+  SEXP call = PROTECT(Rf_lang3(nanoarrow_sym_writebin, chunk_buffer, data->con));
+  while (data->buf_size_bytes > chunk_buffer_size) {
+    memcpy(RAW(chunk_buffer), data->buf, chunk_buffer_size);
+    Rf_eval(call, R_BaseEnv);
+    data->buf_size_bytes -= chunk_buffer_size;
+    data->buf += chunk_buffer_size;
+  }
+
+  UNPROTECT(2);
+
+  // Write remaining bytes
+  if (data->buf_size_bytes > 0) {
+    chunk_buffer = PROTECT(Rf_allocVector(RAWSXP, data->buf_size_bytes));
+    call = PROTECT(Rf_lang3(nanoarrow_sym_writebin, chunk_buffer, data->con));
+    memcpy(RAW(chunk_buffer), data->buf, data->buf_size_bytes);
+    Rf_eval(call, R_BaseEnv);
+    UNPROTECT(2);
+  }
+
+  return R_NilValue;
+}
+
 static ArrowErrorCode read_con_input_stream(struct ArrowIpcInputStream* stream,
                                             uint8_t* buf, int64_t buf_size_bytes,
                                             int64_t* size_read_out,
@@ -129,11 +202,40 @@ static ArrowErrorCode read_con_input_stream(struct ArrowIpcInputStream* stream,
   data.error = error;
   data.return_code = NANOARROW_OK;
 
-  R_tryCatchError(&call_readbin, &data, &handle_readbin_error, &data);
+  R_tryCatchError(&call_readbin, &data, &handle_readbin_writebin_error, &data);
+  return data.return_code;
+}
+
+static ArrowErrorCode write_con_output_stream(struct ArrowIpcOutputStream* stream,
+                                              const void* buf, int64_t buf_size_bytes,
+                                              int64_t* size_write_out,
+                                              struct ArrowError* error) {
+  if (!nanoarrow_is_main_thread()) {
+    ArrowErrorSet(error, "Can't read from R connection on a non-R thread");
+    return EIO;
+  }
+
+  struct ConnectionInputStreamHandler data;
+  data.con = (SEXP)stream->private_data;
+  data.buf = (void*)buf;
+  data.buf_size_bytes = buf_size_bytes;
+  data.size_read_out = NULL;
+  data.error = error;
+  data.return_code = NANOARROW_OK;
+
+  R_tryCatchError(&call_writebin, &data, &handle_readbin_writebin_error, &data);
+
+  // This implementation always blocks until all bytes have been written
+  *size_write_out = buf_size_bytes;
+
   return data.return_code;
 }
 
 static void release_con_input_stream(struct ArrowIpcInputStream* stream) {
+  nanoarrow_release_sexp((SEXP)stream->private_data);
+}
+
+static void release_con_output_stream(struct ArrowIpcOutputStream* stream) {
   nanoarrow_release_sexp((SEXP)stream->private_data);
 }
 
@@ -158,4 +260,18 @@ SEXP nanoarrow_c_ipc_array_reader_connection(SEXP con) {
 
   UNPROTECT(2);
   return array_stream_xptr;
+}
+
+SEXP nanoarrow_c_ipc_output_stream_connection(SEXP con) {
+  SEXP output_stream_xptr = PROTECT(output_stream_owning_xptr());
+  struct ArrowIpcOutputStream* output_stream =
+      (struct ArrowIpcOutputStream*)R_ExternalPtrAddr(output_stream_xptr);
+
+  output_stream->write = &write_con_output_stream;
+  output_stream->release = &release_con_output_stream;
+  output_stream->private_data = (SEXP)con;
+  nanoarrow_preserve_sexp(con);
+
+  UNPROTECT(1);
+  return output_stream_xptr;
 }
