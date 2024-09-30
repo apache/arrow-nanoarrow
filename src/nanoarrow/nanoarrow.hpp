e1545db930cf514d47a2b45d76ea5eb2fc9613c5
Dewey Dunnington <dewey@voltrondata.com>
2024-09-30 17:06:54 -0500

    start again

--- src/nanoarrow/nanoarrow.hpp
+++ src/nanoarrow/nanoarrow.hpp
@@ -15,6 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+#include <cstring>
 #include <exception>
 #include <string>
 #include <vector>
@@ -216,7 +217,10 @@ template <typename T>
 class Unique {
  public:
   /// \brief Construct an invalid instance of T holding no resources
-  Unique() { init_pointer(&data_); }
+  Unique() {
+    std::memset(&data_, 0, sizeof(data_));
+    init_pointer(&data_);
+  }
 
   /// \brief Move and take ownership of data
   Unique(T* data) { move_pointer(data, &data_); }
