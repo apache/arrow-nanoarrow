a3fe767c83f379c622fb07eb395fcd7ac1cb99c2
Dewey Dunnington <dewey@wherobots.com>
2025-03-10 10:48:47 -0500

    with scenarios test

--- examples/cmake-scenarios/build.sh
+++ examples/cmake-scenarios/build.sh
@@ -20,31 +20,28 @@
 set -exuo pipefail
 
 # Build nanoarrow statically.
-cmake -S ../.. -B scratch/nanoarrow_build_static/ \
-    -DCMAKE_INSTALL_PREFIX=scratch/nanoarrow_install_static/ \
+cmake -S ../.. -B scratch/nanoarrow_build/ \
+    -DCMAKE_INSTALL_PREFIX=scratch/nanoarrow_install/ \
     -DNANOARROW_IPC=ON -DNANOARROW_DEVICE=ON -DNANOARROW_TESTING=ON
-cmake --build scratch/nanoarrow_build_static/
-cmake --install scratch/nanoarrow_build_static/
-
-# Build nanoarrow dynamically.
-cmake -S ../.. -B scratch/nanoarrow_build_shared/ \
-    -DCMAKE_INSTALL_PREFIX=scratch/nanoarrow_install_shared/ \
-    -DBUILD_SHARED_LIBS=ON \
-    -DNANOARROW_IPC=ON -DNANOARROW_DEVICE=ON -DNANOARROW_TESTING=ON \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-cmake --build scratch/nanoarrow_build_shared/
-cmake --install scratch/nanoarrow_build_shared/
+cmake --build scratch/nanoarrow_build/
+cmake --install scratch/nanoarrow_build/
 
 for nanoarrow_build_type in static shared; do
     # Build the project against the built nanoarrow.
-    cmake -S . -B scratch/build_${nanoarrow_build_type}/ -Dnanoarrow_ROOT=scratch/nanoarrow_build_${nanoarrow_build_type}/
+    cmake -S . -B scratch/build_${nanoarrow_build_type}/ \
+        -Dnanoarrow_ROOT=scratch/nanoarrow_build \
+        -DTEST_BUILD_TYPE=${nanoarrow_build_type}
     cmake --build scratch/build_${nanoarrow_build_type}/
 
     # Build the project against the installed nanoarrow.
-    cmake -S . -B scratch/build_against_install_${nanoarrow_build_type}/ -Dnanoarrow_ROOT=scratch/nanoarrow_install_${nanoarrow_build_type}/
+    cmake -S . -B scratch/build_against_install_${nanoarrow_build_type}/ \
+        -Dnanoarrow_ROOT=scratch/nanoarrow_install \
+        -DTEST_BUILD_TYPE=${nanoarrow_build_type}
     cmake --build scratch/build_against_install_${nanoarrow_build_type}/
 
     # Now try using FetchContent to get nanoarrow from remote.
-    cmake -S . -B scratch/build_against_fetched_${nanoarrow_build_type}/ -DFIND_NANOARROW=OFF
+    cmake -S . -B scratch/build_against_fetched_${nanoarrow_build_type}/ \
+        -DFIND_NANOARROW=OFF \
+        -DTEST_BUILD_TYPE=${nanoarrow_build_type}
     cmake --build scratch/build_against_fetched_${nanoarrow_build_type}/
 done
