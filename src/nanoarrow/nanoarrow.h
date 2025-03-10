72d5ab28631a7c7bc6549f8eb79ac9e737d907e0
Dewey Dunnington <dewey@wherobots.com>
2025-03-10 11:08:33 -0500

    fix extra dllspec

--- src/nanoarrow/nanoarrow.h
+++ src/nanoarrow/nanoarrow.h
@@ -318,9 +318,8 @@ static inline float ArrowHalfFloatToFloat(uint16_t value);
 /// Given a buffer of increasing int64_t offsets that begin with 0 (e.g., offset buffer
 /// of a large type, run ends of a chunked array implementation), resolve a value v
 /// where lo <= v < hi such that offsets[v] <= index < offsets[v + 1].
-NANOARROW_DLL static inline int64_t ArrowResolveChunk64(int64_t index,
-                                                        const int64_t* offsets,
-                                                        int64_t lo, int64_t hi);
+static inline int64_t ArrowResolveChunk64(int64_t index, const int64_t* offsets,
+                                          int64_t lo, int64_t hi);
 
 /// @}
 
